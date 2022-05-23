044ea3998dade4cadaf923724fcded201b990f01
Benitosam <benito@zionstar.in>
2022-05-23 17:31:22 +0530

    version change

--- db/migrate/20220523114220_create_canvas_oauth_authorized_users.rb
+++ db/migrate/20220523114220_create_canvas_oauth_authorized_users.rb
@@ -1,4 +1,4 @@
-class CreateCanvasOauthAuthorizedUsers < ActiveRecord::Migration[7.0]
+class CreateCanvasOauthAuthorizedUsers < ActiveRecord::Migration[4.2]
   def change
     create_table :canvas_oauth_authorized_users do |t|
       t.integer "user_id"
