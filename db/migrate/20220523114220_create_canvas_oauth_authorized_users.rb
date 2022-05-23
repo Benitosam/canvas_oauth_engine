706603dcb38b51e4330c8d14b532eda982badb0e
Benitosam <benito@zionstar.in>
2022-05-23 17:20:35 +0530

    authorized_user model creation and table creation

--- db/migrate/20220523114220_create_canvas_oauth_authorized_users.rb
+++ db/migrate/20220523114220_create_canvas_oauth_authorized_users.rb
@@ -1,6 +1,10 @@
 class CreateCanvasOauthAuthorizedUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :canvas_oauth_authorized_users do |t|
+      t.integer "user_id"
+      t.integer "course_id"
+      t.string  "user_roll"
+      t.string  "feature_name"
 
       t.timestamps
     end
