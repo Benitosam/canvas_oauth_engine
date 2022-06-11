48a4f4127038b86d52c00efc4c9277885b1b944c
Benitosam <benito@zionstar.in>
2022-06-11 11:20:03 +0530

    Migratin version change

--- db/migrate/20220611054311_add_column_refresh_token_in_authorizations.rb
+++ db/migrate/20220611054311_add_column_refresh_token_in_authorizations.rb
@@ -1,4 +1,4 @@
-class AddColumnRefreshTokenInAuthorizations < ActiveRecord::Migration[7.0]
+class AddColumnRefreshTokenInAuthorizations < ActiveRecord::Migration[4.2]
   def change
     add_column :canvas_oauth_authorizations, :refresh_token, :text
   end
