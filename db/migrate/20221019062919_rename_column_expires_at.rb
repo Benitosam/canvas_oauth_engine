dd190d0e8b9175fa6d78a8a043d91c97fbadeaf2
Benitosam <benito@zionstar.in>
2022-10-19 12:03:56 +0530

    change in version

--- db/migrate/20221019062919_rename_column_expires_at.rb
+++ db/migrate/20221019062919_rename_column_expires_at.rb
@@ -1,4 +1,4 @@
-class RenameColumnExpiresAt < ActiveRecord::Migration[7.0]
+class RenameColumnExpiresAt < ActiveRecord::Migration[4.2]
   def change
     rename_column :canvas_oauth_authorizations, :expires_at, :expires_in
   end
