1e024416bc1c56e8ef1994c4ffab608490af8773
Benitosam <benito@zionstar.in>
2022-10-19 11:40:22 +0530

    Expires in changes

--- db/migrate/20221019055516_change_expires_in_type.rb
+++ db/migrate/20221019055516_change_expires_in_type.rb
@@ -1,5 +1,5 @@
 class ChangeExpiresInType < ActiveRecord::Migration[4.2]
   def change
-    change_column :canvas_oauth_authorizations, :expires_in, :datetime, using: 'expires_in::datetime'
+    change_column :canvas_oauth_authorizations, :expires_in, :datetime, using: 'expires_in::timestamp without time zone'
   end
 end
