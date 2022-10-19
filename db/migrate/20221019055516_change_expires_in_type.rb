fcb19c8faeeef72bdde3cf481713d9ac89784159
Benitosam <benito@zionstar.in>
2022-10-19 11:44:45 +0530

    changes reverted

--- db/migrate/20221019055516_change_expires_in_type.rb
+++ db/migrate/20221019055516_change_expires_in_type.rb
@@ -1,5 +1,5 @@
 class ChangeExpiresInType < ActiveRecord::Migration[4.2]
   def change
-    change_column :canvas_oauth_authorizations, :expires_in, :datetime, using: 'expires_in::timestamp without time zone'
+    change_column :canvas_oauth_authorizations, :expires_in, :datetime, using: 'expires_in::datetime'
   end
 end
